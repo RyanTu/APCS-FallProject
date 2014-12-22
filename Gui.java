14d5282eb52fb59b42b55d72f6c6ca4df2359096
y4smeen <y4smeen@gmail.com>
2014-12-22 08:54:37 -0500

    make gui

---
+++ Gui.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+import java.awt.*;
+import java.swing.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javaz.imageio.*;
+
+public class Gui implements ActionListener {
+    public Gui() {
+	private JFrame frame;
+
+
+    }
+
+    public static void main(String[] args) {
+	new Gui();
+    }
+}
