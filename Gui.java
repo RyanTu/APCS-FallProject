35ce8077e1f3e4d6b1eeeeeee027fc927ac59293
ryan.tu <tu8.rya@gmail.com>
2015-01-06 09:26:00 -0500

    Class

--- Gui.java
+++ Gui.java
@@ -7,26 +7,46 @@ import java.awt.event.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
-public class Gui extends JFrame /* implements ActionListener */ {
+public class Gui extends JFrame{
     
     private JFrame frame;
-    private JPanel pane;
-    private JButton b1, b2;
-    private JLabel mainLabel;
+    private JPanel choose, play1, play2;
+    private JRadioButton A,B,C,D;
+    private JButton[][] grid;
    
     public Gui() {
 	
 	frame = new JFrame();
 	frame.setSize(600,600);
 	frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	frame.setLayout(new FlowLayout());
+	//frame.setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.Y_AXIS));
 
-	pane = new JPanel();
-	pane.setLayout(new GridLayout(9,5));
-	frame.add(pane);
+	choose = new JPanel();
+	choose.setLayout(new GridLayout(1,4));
+	frame.add(choose);
 
-	b1 = new JButton("A");
-	pane.add(b1);
+        A = new JRadioButton("A");
+	choose.add(A);
+        B = new JRadioButton("B");
+	choose.add(B);
+        C = new JRadioButton("C");
+	choose.add(C);
+        D = new JRadioButton("D");
+	choose.add(D);
+
+	//play2 = new JPanel();
+
+	
+	play1 = new JPanel();
+	play1.setLayout(new GridLayout(5,9));
+	grid = new JButton[5][9];
+	for (int y = 0; y < 9; y++){
+	    for (int x = 0; x < 5; x++){
+		grid[x][y] = new JButton();
+		frame.add(grid[x][y]);
+	    }
+	}
+	
 	
 	frame.setVisible(true);
 
