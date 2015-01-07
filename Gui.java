ff71e621fd1fd1d53358df3996abe205cd3507a2
ryan.tu <tu8.rya@gmail.com>
2015-01-07 09:26:25 -0500

    Still working on layout

--- Gui.java
+++ Gui.java
@@ -9,21 +9,22 @@ import javax.imageio.*;
 
 public class Gui extends JFrame{
     
-    private JFrame frame;
+    private JFrame overall;
     private JPanel choose, play1, play2;
     private JRadioButton A,B,C,D;
     private JButton[][] grid;
    
     public Gui() {
 	
-	frame = new JFrame();
-	frame.setSize(600,600);
-	frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	//frame.setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.Y_AXIS));
+	overall = new JFrame();
+	overall.setSize(600,600);
+	overall.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	//overall.setLayout(new BoxLayout(overall,BoxLayout.Y_AXIS));
+	
 
 	choose = new JPanel();
 	choose.setLayout(new GridLayout(1,4));
-	frame.add(choose);
+	//overall.add(choose);
 
         A = new JRadioButton("A");
 	choose.add(A);
@@ -34,21 +35,23 @@ public class Gui extends JFrame{
         D = new JRadioButton("D");
 	choose.add(D);
 
-	//play2 = new JPanel();
+	play2 = new JPanel();
+	play2.setLayout(new GridLayout(5,9));
+	play2.setBorder(BorderFactory.createLineBorder(Color.black));
+	overall.add(play2);
 
-	
-	play1 = new JPanel();
-	play1.setLayout(new GridLayout(5,9));
+	/* Layout issues
+	play1 = new JPanel(new GridLayout(5,9));
 	grid = new JButton[5][9];
 	for (int y = 0; y < 9; y++){
 	    for (int x = 0; x < 5; x++){
 		grid[x][y] = new JButton();
-		frame.add(grid[x][y]);
+		overall.add(grid[x][y]);
 	    }
 	}
+	*/
 	
-	
-	frame.setVisible(true);
+	overall.setVisible(true);
 
     }
 
