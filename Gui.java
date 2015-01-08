2d528f5a608be1d40ac78ffbe3bb06b1bfaffb60
ryan.tu <tu8.rya@gmail.com>
2015-01-08 09:23:35 -0500

    Basic layout made

--- Gui.java
+++ Gui.java
@@ -10,7 +10,8 @@ import javax.imageio.*;
 public class Gui extends JFrame{
     
     private JFrame overall;
-    private JPanel choose, play1, play2;
+    private JPanel choose, play1;
+    private JTable play2;
     private JRadioButton A,B,C,D;
     private JButton[][] grid;
    
@@ -19,12 +20,13 @@ public class Gui extends JFrame{
 	overall = new JFrame();
 	overall.setSize(600,600);
 	overall.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	overall.setLayout(new BorderLayout());
 	//overall.setLayout(new BoxLayout(overall,BoxLayout.Y_AXIS));
 	
 
 	choose = new JPanel();
 	choose.setLayout(new GridLayout(1,4));
-	//overall.add(choose);
+	overall.add(choose, BorderLayout.PAGE_START);
 
         A = new JRadioButton("A");
 	choose.add(A);
@@ -35,22 +37,33 @@ public class Gui extends JFrame{
         D = new JRadioButton("D");
 	choose.add(D);
 
+	/*
 	play2 = new JPanel();
 	play2.setLayout(new GridLayout(5,9));
 	play2.setBorder(BorderFactory.createLineBorder(Color.black));
 	overall.add(play2);
+	*/
 
-	/* Layout issues
+	play2 = new JTable(5,9);
+	for (int i = 0; i < 5; i++) {
+	    play2.setRowHeight(i, 75);
+	}
+	play2.setRowSelectionAllowed(false);
+	overall.add(play2, BorderLayout.CENTER);
+
+	/*
 	play1 = new JPanel(new GridLayout(5,9));
 	grid = new JButton[5][9];
 	for (int y = 0; y < 9; y++){
 	    for (int x = 0; x < 5; x++){
 		grid[x][y] = new JButton();
-		overall.add(grid[x][y]);
+		play1.add(grid[x][y]);
 	    }
 	}
+	overall.add(play1, BorderLayout.CENTER);
 	*/
 	
+	overall.pack();
 	overall.setVisible(true);
 
     }
