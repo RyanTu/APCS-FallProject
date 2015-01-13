7ad3c6fab2017754c4e9286ae7e80b35cf8807bb
Yasmeen Roumie <y4smeen@gmail.com>
2015-01-13 09:27:15 -0500

    base char

--- Base.java
+++ Base.java
@@ -4,11 +4,27 @@ public class Base {
     private int Health, AtkPwr;
     private String Name;
 
-    public void attack(Base other){
+    public Base(String n) {
+	setName(n);
+    }
+
+    public String toString() {
+	return this.getName();
+    }
+
+    public void attack(Base other) {
 	/* Idea: 
 	timer (
 	other.Health -= this.AtkPwr;
 	*/
+    }
+
+    public void setName(String n) {
+	this.n = n;
+    }
+
+    public String getName() {
+	return this.name;
     }
 
 }
