94f88e60f372a5fc60119f35353465dd9cbbfe94
ryan.tu <tu8.rya@gmail.com>
2015-01-13 09:21:44 -0500

    Class

--- Base.java
+++ Base.java
@@ -4,4 +4,11 @@ public class Base {
     private int Health, AtkPwr;
     private String Name;
 
+    public void attack(Base other){
+	/* Idea: 
+	timer (
+	other.Health -= this.AtkPwr;
+	*/
+    }
+
 }
--- ShooterOne.java
+++ ShooterOne.java
@@ -1,3 +1,9 @@
 public class ShooterOne extends Base {
-    
+
+    public ShooterOne(){
+	String Name = "ShooterOne";
+	int Health = 100;
+	int AtkPwr = 10;
+    }
+
 }
